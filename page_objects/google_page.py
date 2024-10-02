59b25ad257d0aa186346e6839a03362aa1124a33
jrivas <jakoc.rivas@outlook.com>
2024-10-02 00:16:31 -0400

    feat: adds new test and page objects necessary to get data from the news google tab

--- page_objects/google_page.py
+++ page_objects/google_page.py
@@ -2,7 +2,7 @@ from page_objects.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys  
 from selenium.common.exceptions import NoSuchElementException
-import time
+from datetime import datetime, timedelta
 
 
   
@@ -15,6 +15,12 @@ class GooglePage(BasePage):
   
     def view_more_button(self):  
         return self.getSelector('#Odp5De  div[role="button"]  span > span:first-child')
+    
+    def news_link(self):
+        return self.getLinkText("News")
+    
+    def news_articles(self):
+        return self.getSelectors("#rso > div > div > div")
         
   
     def search(self, query):  
@@ -27,6 +33,20 @@ class GooglePage(BasePage):
         results = self.getSelectors("div.sinMW")
         filtered_results = [result for result in results if result.text.strip()]
         return filtered_results[:11]
+    
+    def click_news_link(self):
+        self.news_link().click()
+        
+    def get_articles_by_date(self):
+        for article in self.news_articles():
+            title = article.find_element(By.CSS_SELECTOR,'div[role="heading"]').text
+        
+            date_text = article.find_element(By.CSS_SELECTOR, 'div span:nth-child(1)').text
+            date = self.parse_date(date_text)
+            three_months_ago = datetime.now() - timedelta(days=90)
+
+            if date >= three_months_ago:
+              print(f"Title: {title}, Date: {date_text}")
 
   
   
--- specs/search/google_test.py
+++ specs/search/google_test.py
@@ -3,20 +3,38 @@ from page_objects.google_page import GooglePage
 import time
 
   
+# @pytest.mark.search  
+# def test_google_search_functionality(chrome_browser):  
+#     """  
+#     Test the search functionality of google
+#     """  
+#     url = "https://google.com"  
+#     google_page = GooglePage(chrome_browser)  
+  
+#     google_page.open(url)  
+  
+#     google_page.search("QA Automation Tools")
+#     time.sleep(3)  
+#     search_results = google_page.get_search_results()
+#     for index, result in enumerate(search_results, 0):
+#       print(f"{index}. {result.text}")
+  
 @pytest.mark.search  
-def test_google_search_functionality(chrome_browser):  
+def test_google_news_tab(chrome_browser):  
     """  
-    Test the search functionality of google
+    Test the news tab functionality of google
     """  
-    url = "https://google.com"  
+    url = "https://google.com?hl=en"  
     google_page = GooglePage(chrome_browser)  
   
     google_page.open(url)  
   
     google_page.search("QA Automation Tools")
     time.sleep(3)  
-    search_results = google_page.get_search_results()
-    for index, result in enumerate(search_results, 0):
-      print(f"{index}. {result.text}")
+    google_page.click_news_link()
+    # time.sleep(3)  
+
+    google_page.get_articles_by_date()
+
   
 
