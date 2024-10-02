5b8df275ace1c027a480c9738b46074b6aac2f6d
anon <anon@anondebian.mmmiflx1.pop.starlinkisp.net>
2024-10-01 22:16:21 -0400

    feat: adds pytest config file to setup pytest with chromedriver

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest  
+from selenium import webdriver  
+from selenium.webdriver.chrome.service import Service as ChromeService  
+from webdriver_manager.chrome import ChromeDriverManager  
+  
+  
+@pytest.fixture()  
+def chrome_browser():  
+    driver = webdriver.Chrome()  
+  
+    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  
+      
+    driver.implicitly_wait(10)  
+    # Yield the WebDriver instance  
+    yield driver  
+    # Close the WebDriver instance  
+    driver.quit()
