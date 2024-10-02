d38cb4ee710c015d9c24fc9b941a9b3c2e8f0723
jrivas <jakoc.rivas@outlook.com>
2024-10-02 00:15:12 -0400

    feat: adds new chrome options

--- conftest.py
+++ conftest.py
@@ -1,14 +1,21 @@
 import pytest  
 from selenium import webdriver  
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService  
 from webdriver_manager.chrome import ChromeDriverManager  
   
   
 @pytest.fixture()  
 def chrome_browser():  
-    driver = webdriver.Chrome()  
+    options = Options()
+    options.add_argument("--lang=en")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    # driver = webdriver.Chrome(options=options)  
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)  
+
+    driver.delete_all_cookies()
   
-    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  
       
     driver.implicitly_wait(10)  
     # Yield the WebDriver instance  
