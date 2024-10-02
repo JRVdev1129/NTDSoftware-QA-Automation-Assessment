302bc8644cb94c7e0394f19556ad06cb7e655178
jrivas <jakoc.rivas@outlook.com>
2024-10-02 11:46:59 -0400

    feat: adds code to wait for the element to be displayed before doing any interactions

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -3,6 +3,8 @@ Base Page object
 """  
   
 from selenium.webdriver.common.by import By  
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import dateparser
 
   
@@ -13,17 +15,23 @@ class BasePage:
     def open(self, url):  
         self.driver.get(url)  
         
-    def getSelector(self, locator):
-        return self.driver.find_element(By.CSS_SELECTOR, locator)
+    def get_selector(self, locator):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, locator)))
+        return element
     
-    def getSelectors(self, locator):
+    def get_selectors(self, locator):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, locator)))
         return self.driver.find_elements(By.CSS_SELECTOR, locator)
     
-    def getName(self, text):
+    def get_name(self, text):
         return self.driver.find_element(By.NAME, text)
     
-    def getLinkText(self, text):
-        return self.driver.find_element(By.LINK_TEXT, text)
+    def get_link_text(self, text):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, text)))
+        return element
     
     def parse_date(self, date):
         parsed_date = dateparser.parse(date)
